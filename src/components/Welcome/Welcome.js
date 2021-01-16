b2b84382cb68482c199714c4f2086f069c5f702c
bevhatzl <beverleyhatzl@gmail.com>
2021-01-16 14:42:39 +1100

    Welcome screen without photo

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import "./style.css";
+
+// Display the Welcome screen
+function Welcome () {
+    return (
+        <div class="masthead bg-primary text-white text-center">
+        <div class="container d-flex align-items-center flex-column">
+
+            <img class="masthead-avatar mb-5" src="../../../public/images/avatar.jpeg" alt="photo of me" />
+
+            <h1 class="masthead-heading text-uppercase mb-0">Welcome!</h1>
+   
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+   
+            <p class="masthead-subheading font-weight-light mb-0">Hi, I'm Beverley Hatzl. I'm a Junior Web Developer,
+                based in Sydney, seeking my first dev job.</p>
+        </div>
+    </div>
+    )
+}
+
+export default Welcome;
