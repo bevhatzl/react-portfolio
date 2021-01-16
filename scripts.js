c1e1965d00fc17bf1a18417fc771397813ccac48
bevhatzl <beverleyhatzl@gmail.com>
2021-01-17 00:09:43 +1100

    Updates

--- scripts.js
+++ scripts.js
@@ -28,7 +28,8 @@
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
       $('.navbar-collapse').collapse('hide');
-      window.scrollTo(0, 0);;
+      console.log('Scroll to top');
+      window.scrollTo(0, 0);
     });
   
     // Activate scrollspy to add active class to navbar items on scroll
