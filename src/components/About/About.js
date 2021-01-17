ea5f26dd1bff1a761835ca7efa5fc80854c8c8a1
bevhatzl <beverleyhatzl@gmail.com>
2021-01-17 13:44:17 +1100

    change resume link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,21 +17,21 @@ function About () {
                     <p className="lead" id="strongFont">
                         I have recently completed the ​Full Stack Coding Boot Camp at University of Sydney. Now I am
                         looking
-                        for a Junior position as Backend, but I am also keen to work as Frontend.​ I first started a
+                        for a Junior position as Back End, but I am also keen to work as Front End Web Developer.​ I first started a
                         passion for programming while studying for my Bachelor of Business in the past. Now since
                         January
                         2020 I have started self study to learn Web Development with the aim to become a Junior
-                        Developer. Since completing my formal studies I am starting more projects to be completed in my
-                        own time.
+                        Developer. 
                     </p>
                 </div>
                 <div className="col-lg-4 mr-auto">
-                    <p className="lead" id="strongFont">I am proficient with HTML, CSS, JavaScript, jQuery, Bootstrap, Git, Node,
+                    <p className="lead" id="strongFont">Since completing my formal studies I am starting more projects to be completed in my
+                        own time. I am proficient with HTML, CSS, JavaScript, jQuery, Bootstrap, Git, Node,
                         Express.js, SQL, MongoDB, ORMs, ODMs and React. This portfolio was built using React!
 
                         Check out my projects in the Portfolio and feel free to get in touch with me. You can view my
                         resume <a
-                            href="https://drive.google.com/file/d/19U3Jp3mZhTqVvSSfVk1HWbZwEcATvKmP/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1eor7fu5fDRdaIAJhHThCY63yd_8UAZ21/view?usp=sharing"
                             target="_blank" id="resume-link">here.</a>
                     </p>
                 </div>
