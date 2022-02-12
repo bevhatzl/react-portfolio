170c70a1f83f4d1fae0c29bca7e8e7fd7237b82b
bevhatzl <beverleyhatzl@gmail.com>
2022-02-12 23:35:31 +1100

    update description and link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,13 +15,8 @@ function About () {
             <div className="row">
                 <div className="col-lg-4 ml-auto">
                     <p className="lead" id="strongFont">
-                        I have recently completed the ​Full Stack Coding Boot Camp at University of Sydney. Now I am
-                        looking
-                        for a Junior position as Back End, but I am also keen to work as Front End Web Developer.​ I first started a
-                        passion for programming while studying for my Bachelor of Business in the past. Now since
-                        January
-                        2020 I have started self study to learn Web Development with the aim to become a Junior
-                        Developer. 
+                        I have completed the ​Full Stack Coding Boot Camp at University of Sydney. I enjoy both Back End or Front End Web.​ I first started a
+                        passion for programming while studying for my Bachelor of Business in the past. Now most recently I have started self study to learn Web Development with the aim to become a full stack engineer. 
                     </p>
                 </div>
                 <div className="col-lg-4 mr-auto">
@@ -31,7 +26,7 @@ function About () {
 
                         Check out my projects in the Portfolio and feel free to get in touch with me. You can view my
                         resume <a
-                            href="https://drive.google.com/file/d/1eor7fu5fDRdaIAJhHThCY63yd_8UAZ21/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1xhJ2ltSVOmlAjX7vo-pIXoOAGPv1OJmW/view?usp=sharing"
                             target="_blank" id="resume-link">here.</a>
                     </p>
                 </div>
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -14,7 +14,7 @@ function Welcome () {
                     <div className="divider-custom-line"></div>
                 </div>
                 <p className="masthead-subheading mb-0">Hi, I'm Beverley Hatzl. I'm a Junior Web Developer,
-                based in Sydney, seeking my first dev job.</p>
+                based in Sydney.</p>
             </div>
         </div>
     )
